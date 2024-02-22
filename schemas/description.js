c74d503f0b2055bcb78e50efb843847f8cfbd51d
Lakshmanan <lakshmangri98@gmail.com>
2024-02-22 13:11:25 +0530

    Updated submodule backend sanity

---
+++ schemas/description.js
@@ -0,0 +1,11 @@
+export default {
+    name:'description',
+    title:'Description',
+    type:'document',
+    fields:[
+           {name:'desc',
+               title:'desc',
+               type:'string'
+            },
+    ]
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+// Import the Schemas you create
+import testimonials from "./testimonials"
+import about from "./about"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
+import description from "./description"
+import contact from "./contact"
+
+// Then, we give our schema to the builder and provide the result to Sanity
+export const schemaTypes = [about, skills, works, workExperience, experiences, description, testimonials, contact]
+
+
+
